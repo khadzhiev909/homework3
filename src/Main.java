30ea7dabbd70a1a1935e03d9f42a909baaca9b4b
Эпенди <khadzhiev909@gmail.com>
2023-02-08 19:32:24 +0300

    homework3

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+//        task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
+    }
+
+
+}
